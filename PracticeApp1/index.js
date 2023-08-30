3bc0b8b1f2d85300c06eb6030f06c72670fae878
Runia Tanbin <runia5036@gmail.com>
2023-08-30 13:43:20 +0600

    express first app

---
+++ PracticeApp1/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+app = express();
+
+app.get("/",function (req,res){
+    res.send("hello express js");
+});
+
+app.listen(8000, function(){
+    console.log("server run success")
+});
